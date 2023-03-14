5ffc864996bef41941d0b276bf2ddf382dc92808
Ezequiel Rivera <114954551+EzequielUno@users.noreply.github.com>
2023-03-14 17:16:54 -0300

    Subida forzada

---
+++ scripts/script.js
@@ -0,0 +1,63 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+});
+
+sr.reveal('#img-1', {
+    duration: 2000,
+    origin: 'rigth',
+    distance: '300px',
+    delay: 1000
+});
+
+sr.reveal('#text-1', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+
+sr.reveal('#text-2', {
+    duration: 2000,
+    origin: 'rigth',
+    distance: '300px',
+    delay: 1000
+});
+
+sr.reveal('#img-2', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+
+sr.reveal('#img-3-i', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    delay: 1000
+});
+
+sr.reveal('#img-3-d', {
+    duration: 2000,
+    origin: 'rigth',
+    distance: '300px',
+    delay: 1500
+});
+
+sr.reveal('#text-3', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+
+// SCROLL LENTO //
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
